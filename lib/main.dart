002a4803b052e8f0b674e10d17c10484c7f2dd03
陈博宇 <terry.chen@cloudkeeper.cn>
2019-05-10 10:13:11 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('电影列表'),
+        centerTitle: true,
+        // 右侧行为按钮
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),onPressed: (){},)
+        ],
+      ),
+      body: Text('33'),
+      drawer: Drawer(),
+    );
+  }
+}
